811cbd5ff1cb550b6a00aa964f26c4d9829c973e
i.demchenko <@_i@#KPO23KJte>
2025-12-03 23:16:36 +0300

    chore: правка в шаблон

--- resources/views/b24api/install.blade.php
+++ resources/views/b24api/install.blade.php
@@ -8,15 +8,6 @@
         BX24.init(function () {
             BX24.installFinish();
         });
-        BX24.ready(async function () {
-          await BX24.init(async function () {
-            window.axios.defaults.headers.common['X-b24api-access-token'] = BX24.getAuth().access_token;
-            window.axios.defaults.headers.common['X-b24api-refresh-token'] = BX24.getAuth().refresh_token;
-            window.axios.defaults.headers.common['X-b24api-domain'] = BX24.getAuth().domain;
-            window.axios.defaults.headers.common['X-b24api-member-id'] = BX24.getAuth().member_id;
-            window.axios.defaults.headers.common['X-b24api-expires-in'] = BX24.getAuth().expires_in;
-          });
-        });
     </script>
 </head>
 <body>
