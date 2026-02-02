5b9dd96da432956c0b2adc7121fb1231f456a57a
i.demchenko <@_i@#KPO23KJte>
2026-02-02 12:02:26 +0300

    chore: доработки

--- resources/js/components/TimeList.vue
+++ resources/js/components/TimeList.vue
@@ -188,7 +188,7 @@
                                     <div class="flex items-center space-x-2 min-w-0">
                                       <B24Badge
                                           :class="getCategoryBadgeClass(categoryData.category)"
-                                          class="whitespace-normal md:whitespace-nowrap align-middle inline-block text-xs"
+                                          class="whitespace-normal md:whitespace-nowrap max-w-full inline-block px-2 py-2 text-xs"
                                       >
                                         {{ categoryData.category || 'Не указана' }}
                                       </B24Badge>
@@ -511,7 +511,7 @@
                               <div class="flex items-center space-x-2 min-w-0">
                                 <B24Badge
                                     :class="getCategoryBadgeClass(categoryData.category)"
-                                    class="whitespace-normal md:whitespace-nowrap align-middle inline-block text-xs"
+                                    class="whitespace-normal md:whitespace-nowrap max-w-full inline-block px-2 py-2 text-xs"
                                 >
                                   {{ categoryData.category || 'Не указана' }}
                                 </B24Badge>
