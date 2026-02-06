365d918b42c87485756b2cafc51368121e4b674a
i.demchenko <@_i@#KPO23KJte>
2026-02-06 13:50:02 +0300

    chore: доработки

--- resources/js/components/WorkDayStatistics.vue
+++ resources/js/components/WorkDayStatistics.vue
@@ -544,7 +544,7 @@
               </div>
 
               <!-- Сообщение об отсутствии данных -->
-              <div v-else-if="!isLoading && !hasData" class="text-center py-8 md:py-12 text-gray-500">
+              <div v-if="!isLoading && !hasData" class="text-center py-8 md:py-12 text-gray-500">
                 <svg class="w-12 h-12 md:w-16 md:h-16 mx-auto mb-3 md:mb-4 text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
                 </svg>
