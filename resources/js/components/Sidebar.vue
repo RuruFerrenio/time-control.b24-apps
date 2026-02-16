051083e58a94bf0c0c87abe7b0e478ec921feb1a
i.demchenko <@_i@#KPO23KJte>
2026-02-16 15:00:13 +0300

    chore: доработки

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -178,11 +178,8 @@
                 color="air-secondary"
                 size="md"
             >
-              <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <!-- Жирный внешний круг -->
-                <circle cx="12" cy="12" r="9" stroke="currentColor" stroke-width="1.8" fill="none"/>
-                <!-- Жирная буква Р с перечеркиванием (рубль) -->
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.8" d="M9 8.5h4.2a2.3 2.3 0 1 1 0 4.6H9 M9 13.1h2.8a2.3 2.3 0 1 1 0 4.6H9 M12 6v12" />
+              <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
               </svg>
               Поддержать проект
             </B24Button>
