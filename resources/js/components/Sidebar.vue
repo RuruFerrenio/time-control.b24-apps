10d896ff389bf6918f6884881b2b8fe33ec15911
i.demchenko <@_i@#KPO23KJte>
2026-02-16 14:58:26 +0300

    chore: доработки

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -179,7 +179,10 @@
                 size="md"
             >
               <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z M20 12c0 4.42-3.58 8-8 8s-8-3.58-8-8 3.58-8 8-8 8 3.58 8 8z M12 6v2 M12 16v2 M10 9h3.5a1.5 1.5 0 0 1 0 3H10h1.5a1.5 1.5 0 0 1 0 3H10"></path>
+                <!-- Внешний круг монеты -->
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" />
+                <!-- Буква Р (рубль) -->
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 8h4.5a2.5 2.5 0 0 1 0 5H9h3.5a2.5 2.5 0 0 1 0 5H9 M12 6v12" />
               </svg>
               Поддержать проект
             </B24Button>
