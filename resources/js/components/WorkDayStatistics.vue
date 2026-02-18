df0dd4aef8716a8778f50c60a2a5b1d679324d81
i.demchenko <@_i@#KPO23KJte>
2026-02-18 10:42:52 +0300

    chore: доработки

--- resources/js/components/WorkDayStatistics.vue
+++ resources/js/components/WorkDayStatistics.vue
@@ -116,7 +116,7 @@
                             Распределение времени
                           </span>
                         </h4>
-                        <div class="relative w-full h-64">
+                        <div class="relative w-full h-84">
                           <canvas ref="bitrixTimeChart"></canvas>
                           <!-- Центральный текст -->
                           <div class="absolute inset-0 flex flex-col items-center justify-center pointer-events-none">
