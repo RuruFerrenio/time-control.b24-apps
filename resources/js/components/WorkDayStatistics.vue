8b939514ebe9f1283533a3052fb3668ead7da7ae
i.demchenko <@_i@#KPO23KJte>
2026-02-25 10:13:20 +0300

    chore: доработки

--- resources/js/components/WorkDayStatistics.vue
+++ resources/js/components/WorkDayStatistics.vue
@@ -38,7 +38,7 @@
                     </svg>
                     Обновить
                   </B24Button>
-                  <B24Button
+                  <!--<B24Button
                       @click="exportToPDF"
                       :disabled="isLoading"
                       color="air-secondary"
@@ -49,7 +49,7 @@
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
                     </svg>
                     Экспорт PDF
-                  </B24Button>
+                  </B24Button> -->
                 </div>
               </div>
 
