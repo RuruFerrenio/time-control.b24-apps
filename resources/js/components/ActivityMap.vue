fb0613b8f5b55adfcf3fa51af5ed40b092b0abd8
i.demchenko <@_i@#KPO23KJte>
2026-02-26 14:07:45 +0300

    chore: доработки

--- resources/js/components/ActivityMap.vue
+++ resources/js/components/ActivityMap.vue
@@ -33,6 +33,18 @@
                     </svg>
                     Обновить
                   </B24Button>
+                  <B24Button
+                      @click=""
+                      :disabled="isLoading"
+                      color="air-secondary"
+                      size="sm"
+                      class="flex-1 w-full sm:w-auto justify-center"
+                  >
+                    <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
+                    </svg>
+                    Экспорт XLS
+                  </B24Button>
                 </div>
               </div>
 
