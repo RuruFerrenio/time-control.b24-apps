4d87bcaddc881bc22eff54917d76195a51761ea0
i.demchenko <@_i@#KPO23KJte>
2026-03-02 22:52:25 +0300

    chore: доработки

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -53,11 +53,6 @@
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"/>
                 </svg>
                 Плагин для браузера
-
-                <!-- Всплывающая подсказка при наведении -->
-                <div class="absolute left-0 top-full mt-1 px-2 py-1 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap z-10">
-                  В следующей версии...
-                </div>
               </div>
 
               <!-- Ссылка на статистику рабочего дня с проверкой тарифа -->
