75c244e02ed2e16bb97f67854b604a1906ca1acc
i.demchenko <@_i@#KPO23KJte>
2026-03-06 13:16:22 +0300

    chore: доработки

--- app/Http/Controllers/Bitrix24/AppController.php
+++ app/Http/Controllers/Bitrix24/AppController.php
@@ -9,36 +9,28 @@ use X3Group\Bitrix24\Bitrix24ApiClient;
 
 class AppController extends Controller
 {
-	public function index(Bitrix24ApiClient $bitrix24, Request $request): View
-	{
-		// Получаем параметры авторизации из URL
-		$authParams = [
-			'access_token' => $request->input('AUTH_ID') ?? $request->input('code'),
-			'refresh_token' => $request->input('REFRESH_ID'),
-			'domain' => $request->input('domain'),
-			'member_id' => $request->input('member_id'),
-			'expires_in' => $request->input('expires_in'),
-			'state' => $request->input('state'),
-		];
+    public function index(Bitrix24ApiClient $bitrix24, Request $request): View
+    {
+	    $placementOptions = $request->input('PLACEMENT_OPTIONS', '{}');
+	    $params = $request->input('params', []);
 
-		$placementOptions = $request->input('PLACEMENT_OPTIONS', '{}');
-		$params = $request->input('params', []);
+	    $options = json_decode($placementOptions, true) ?? [];
 
-		$options = json_decode($placementOptions, true) ?? [];
-
-		$parameters = [];
 		if(isset($options['parameters'])){
 			$parameters = json_decode($options['parameters'], true) ?? [];
 		}
 
-		$mode = $parameters['mode'] ?? null;
+	    $mode = $parameters['mode'] ?? null;
 
-		return view('b24api.index', [
-			'authParams' => $authParams,
-			'placementOptions' => $placementOptions,
-			'mode' => $mode,
-			'params' => $params,
-			'fitWindow' => $request->input('fitWindow', false),
+        return view('b24api.index', [
+	        'placementOptions' => $placementOptions,
+	        'mode' => $mode,
+        ]);
+    }
+
+	public function oauth(Bitrix24ApiClient $bitrix24, Request $request): View
+	{
+		return view('b24api.oauth', [
 		]);
 	}
 }
--- resources/views/b24api/index.blade.php
+++ resources/views/b24api/index.blade.php
@@ -1,53 +1,28 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <script src="//api.bitrix24.com/api/v1/"></script>
-    <title>Приложение</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    {{-- Добавляем meta-теги с токенами для безопасности --}}
-    <meta name="api-access-token" content="{{ $authParams['access_token'] ?? '' }}">
-    <meta name="api-refresh-token" content="{{ $authParams['refresh_token'] ?? '' }}">
-    <meta name="api-domain" content="{{ $authParams['domain'] ?? '' }}">
-    <meta name="api-member-id" content="{{ $authParams['member_id'] ?? '' }}">
-    <meta name="api-expires-in" content="{{ $authParams['expires_in'] ?? '' }}">
-
-    <script>
-
-      console.log('!!!!!!!!!!!!!!!!');
-      console.log(@json($authParams ?? []))
-      window.bitrixData = {
-        params: @json($params ?? null),
-        mode: @json($mode ?? null),
-        placementOptions: @json($placementOptions ?? []),
-        fitWindow: @json($fitWindow ?? []),
-        // Добавляем authParams для доступа из JavaScript
-        auth: @json($authParams ?? [])
-      };
-    </script>
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <script>
-      BX24.init(function () {
-        BX24.installFinish();
-      });
-
-      BX24.ready(async function () {
-        await BX24.init(async function () {
-          window.axios.defaults.headers.common['X-b24api-access-token'] = BX24.getAuth().access_token;
-          window.axios.defaults.headers.common['X-b24api-refresh-token'] = BX24.getAuth().refresh_token;
-          window.axios.defaults.headers.common['X-b24api-domain'] = BX24.getAuth().domain;
-          window.axios.defaults.headers.common['X-b24api-member-id'] = BX24.getAuth().member_id;
-          window.axios.defaults.headers.common['X-b24api-expires-in'] = BX24.getAuth().expires_in;
-        });
-      });
-    </script>
-</head>
-<body>
-<div id="app" class="isolate">
-    <!-- Vue приложение будет монтироваться сюда -->
-</div>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <script src="//api.bitrix24.com/api/v1/"></script>
+        <title>Приложение</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script>
+          window.bitrixData = {
+            params: @json($params ?? null),
+            mode: @json($mode ?? null),
+            placementOptions: @json($placementOptions ?? []),
+            fitWindow: @json($fitWindow ?? []),
+          };
+        </script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script>
+          BX24.init(function () {
+            BX24.installFinish();
+          });
+        </script>
+    </head>
+    <body>
+        <div id="app" class="isolate">
+            <!-- Vue приложение будет монтироваться сюда -->
+        </div>
+    </body>
 </html>
