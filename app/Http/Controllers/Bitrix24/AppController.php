7c1d193811bcf8a0c8ba5574df17cfd835535af5
i.demchenko <@_i@#KPO23KJte>
2026-03-06 16:10:48 +0300

    chore: доработки

--- app/Http/Controllers/Bitrix24/AppController.php
+++ app/Http/Controllers/Bitrix24/AppController.php
@@ -9,60 +9,22 @@ use X3Group\Bitrix24\Bitrix24ApiClient;
 
 class AppController extends Controller
 {
-    public function index(Bitrix24ApiClient $bitrix24, Request $request): View
-    {
-	    $placementOptions = $request->input('PLACEMENT_OPTIONS', '{}');
-	    $params = $request->input('params', []);
-
-	    $options = json_decode($placementOptions, true) ?? [];
-
-		if(isset($options['parameters'])){
-			$parameters = json_decode($options['parameters'], true) ?? [];
-		}
-
-	    $mode = $parameters['mode'] ?? null;
-
-        return view('b24api.index', [
-	        'placementOptions' => $placementOptions,
-	        'mode' => $mode,
-        ]);
-    }
-
-	public function oauth(Request $request): View
+	public function index(Bitrix24ApiClient $bitrix24, Request $request): View
 	{
-		// Получаем параметры авторизации из URL
-		$authParams = [
-			'access_token' => $request->input('AUTH_ID') ?? $request->input('code'),
-			'refresh_token' => $request->input('REFRESH_ID'),
-			'domain' => $request->input('domain'),
-			'member_id' => $request->input('member_id'),
-			'expires_in' => $request->input('expires_in'),
-			'state' => $request->input('state'),
-		];
-
-		print_r($authParams);
-
-		// Для отладки - запишем в лог
-		\Log::info('OAuth params:', $authParams);
-
 		$placementOptions = $request->input('PLACEMENT_OPTIONS', '{}');
 		$params = $request->input('params', []);
 
 		$options = json_decode($placementOptions, true) ?? [];
 
-		$parameters = [];
 		if(isset($options['parameters'])){
 			$parameters = json_decode($options['parameters'], true) ?? [];
 		}
 
 		$mode = $parameters['mode'] ?? null;
 
-		return view('b24api.oauth', [ // Возвращаем тот же шаблон, но с authParams
-          'authParams' => $authParams,
-          'placementOptions' => $placementOptions,
-          'mode' => $mode,
-          'params' => $params,
-          'fitWindow' => $request->input('fitWindow', false),
+		return view('b24api.index', [
+			'placementOptions' => $placementOptions,
+			'mode' => $mode,
 		]);
 	}
 }
--- routes/b24appFrontRequest.php
+++ routes/b24appFrontRequest.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\Bitrix24\AppController;
 use Illuminate\Support\Facades\Route;
 
 /**
  * Запросы из фронта приложения. Обязательна передача авторизации через
  * header X-b24api-access-token, X-b24api-member-id, X-b24api-domain.
  */
-Route::get('/app', [AppController::class, 'oauth']);
