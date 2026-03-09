78fd823d209001b77cfe6a8fc88f38b1b4fc88a7
i.demchenko <@_i@#KPO23KJte>
2026-03-09 17:01:38 +0300

    chore: доработки

--- resources/js/components/ActivityMap.vue
+++ resources/js/components/ActivityMap.vue
@@ -100,7 +100,7 @@
                         :disabled="isLoading"
                         color="air-secondary-accent"
                         size="lg"
-                        class="!w-10 !h-10 !min-w-0 !p-0 flex items-center justify-center"
+                        class="w-10 h-10 p-0 flex items-center justify-center"
                         :title="'Сбросить фильтр по дате'"
                     >
                       <svg
@@ -108,7 +108,7 @@
                           :class="{ 'animate-spin': isLoading }"
                           fill="none"
                           stroke="currentColor"
-                          viewBox="0 0 24 24"
+                          viewBox="0 0 10 10"
                       >
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                       </svg>
