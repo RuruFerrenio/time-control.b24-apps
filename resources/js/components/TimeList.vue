435269e218dfb58ee60f264efea365c8c2889f6b
i.demchenko <@_i@#KPO23KJte>
2026-03-10 20:33:02 +0300

    chore: доработки

--- resources/js/components/TimeList.vue
+++ resources/js/components/TimeList.vue
@@ -419,7 +419,7 @@
                                 class="truncate overflow-visible"
                             />
                             <!-- Кнопка запроса отчета (только для других пользователей, если включено в настройках) -->
-                            <div v-if="userData.userId !== currentUserId && subordinateReportsEnabled" class="ml-2 hidden sm:block">
+                            <div v-if="userData.userId !== currentUserId && subordinateReportsEnabled" class="flex justify-center hidden sm:block">
                               <B24Button
                                   @click="showRequestReportModal(userData)"
                                   size="sm"
@@ -427,7 +427,7 @@
                                   title="Запросить отчет"
                               >
                                 <svg class="w-3 h-3 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
+                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2-2z"/>
                                 </svg>
                                 Запросить отчет
                               </B24Button>
