e3fe431ff38f817a4d216a91944ef136d4c34841
i.demchenko <@_i@#KPO23KJte>
2026-03-13 21:12:06 +0300

    chore: доработки

--- resources/js/components/TimeList.vue
+++ resources/js/components/TimeList.vue
@@ -5,7 +5,7 @@
         description="Иерархичное отображение статистики посещений страниц сотрудниками"
     />
 
-    <div class="mt-0 md:mt-4 grid grid-cols-1 lg:grid-cols-3 gap-6 md:gap-8">
+    <div class="mt-0 md:mt-2 grid grid-cols-1 lg:grid-cols-3 gap-6 md:gap-8">
       <div class="lg:col-span-2">
         <!-- Иерархичный История посещений -->
         <B24Card>
