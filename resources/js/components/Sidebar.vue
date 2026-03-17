fde2592e308e9edd0b10044d0d885065dfb6ba71
i.demchenko <@_i@#KPO23KJte>
2026-03-17 11:10:12 +0300

    chore: доработки

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -237,8 +237,6 @@
                 class="w-full justify-center"
                 color="air-primary"
                 size="md"
-                title="Скоро..."
-                disabled=""
             >
               <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"/>
@@ -351,8 +349,14 @@ export default {
     }
 
     const handleReview = () => {
-      // TODO: Реализовать логику отзыва
-    }
+      if (window.BX24) {
+        BX24.init(() => {
+          BX24.openPath('/market/detail/tekhnogalera.chistoe_vremya/');
+        });
+      } else {
+        console.error('BX24 API не загружен');
+      }
+    };
 
     // Слушатель событий обновления времени
     const handleTimeUpdate = (event) => {
